c63f46abf3ad813dd916f3dc789c2554eeb2a081
Gabriela.Prieto.Herrera <gabiprieto7@gmail.com>
2023-01-20 20:30:33 +0000

    Creada persona e imprimida

--- _01_PrimerProyectoGit/src/git/main/Main.java
+++ _01_PrimerProyectoGit/src/git/main/Main.java
@@ -1,11 +1,18 @@
 package git.main;
 
+import git.entidad.Persona;
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Bienvenidos a nuestras versiones con GIT");
         System.out.println("cambio");
 		
+		Persona p = new Persona();
+		p.setNombre("Margarita");
+		p.setEdad(52);
+		
+		System.out.println(p);
 		
 	}
 
